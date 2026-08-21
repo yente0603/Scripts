2f4dec28b042476732fd15f0d2dcbf2bd77b4605
ADVANTECH\Jasper.Lee <jasper.lee@advantech.com>
2026-08-21 14:04:34 +0800

    Update scripts

--- common/docker_install.sh
+++ common/docker_install.sh
@@ -24,6 +24,7 @@ sudo apt update
 
 # Install the Docker packages
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker $USER
 sudo systemctl start docker
 
 # Verify that the installation is successful by running the hello-world image
--- common/setsudo.sh
+++ common/setsudo.sh
@@ -1,1 +1,4 @@
-sudo grep $USER /etc/sudoers | grep -cq NOPASSWD || sudo su -c 'echo $SUDO_USER ALL=\(ALL:ALL\) NOPASSWD\:ALL >> /etc/sudoers'
+#!/bin/bash
+
+[[ $EUID -eq 0 ]] && { echo "Please run this script without sudo."; exit 1 }
+sudo grep $USER /etc/sudoers | grep -cq NOPASSWD || sudo su -c 'echo ${SUDO_USER} ALL=\(ALL:ALL\) NOPASSWD\:ALL >> /etc/sudoers'
